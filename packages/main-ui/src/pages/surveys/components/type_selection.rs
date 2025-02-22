6ee0b6d331b62c96a28c27992e2c28d471d75533
victor138128 <yepp0517@naver.com>
2025-02-22 19:17:52 +0900

    fix survey page bugfix

--- packages/main-ui/src/pages/surveys/components/type_selection.rs
+++ packages/main-ui/src/pages/surveys/components/type_selection.rs
@@ -4,8 +4,19 @@ use dioxus_translate::Language;
 use models::Question;
 
 #[component]
-pub fn QuestionTypeSelector(lang: Language, onchange: EventHandler<String>) -> Element {
-    let mut selected_type = use_signal(move || Question::default().to_type(&lang));
+pub fn QuestionTypeSelector(
+    lang: Language,
+    selected: String,
+    onchange: EventHandler<String>,
+) -> Element {
+    let mut selected_type = use_signal({
+        let selected = selected.clone();
+        move || selected.clone()
+    });
+
+    use_effect(use_reactive(&selected.clone(), move |selected| {
+        selected_type.set(selected);
+    }));
 
     rsx! {
         select {
