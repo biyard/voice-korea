ba04f1a8094c0429f921f1f16db50d6dd27a6ec8
Boniface <boniface.ebuka@gmail.com>
2025-03-05 01:41:38 +0100

    Added the functionality for creating, reading, updating and deleting a review

--- packages/api/src/controllers/reviews/v1/mod.rs
+++ packages/api/src/controllers/reviews/v1/mod.rs
@@ -7,6 +7,7 @@ use by_axum::axum::{
 use models::{
     v2::{
         Review, ReviewAction, ReviewByIdAction, ReviewCreateRequest, ReviewGetResponse,
+        ReviewSummary,
         ReviewParam, ReviewQuery, ReviewQueryActionType, ReviewRepository, ReviewUpdateRequest,
     },
     *,
@@ -15,12 +16,13 @@ use models::{
 #[derive(Clone, Debug)]
 pub struct ReviewControllerV1 {
     review_repo: ReviewRepository,
+    pool: sqlx::Pool<sqlx::Postgres>,
 }
 
 impl ReviewControllerV1 {
     pub fn route(pool: sqlx::Pool<sqlx::Postgres>) -> Result<by_axum::axum::Router> {
         let review_repo = Review::get_repository(pool.clone());
-        let ctrl = ReviewControllerV1 { review_repo };
+        let ctrl = ReviewControllerV1 { review_repo, pool };
 
         Ok(by_axum::axum::Router::new()
             .route("/", post(Self::act_review).get(Self::list_reviews))
@@ -32,11 +34,21 @@ impl ReviewControllerV1 {
         State(ctrl): State<ReviewControllerV1>,
         Path(id): Path<i64>,
     ) -> Result<Json<Review>> {
-        //TODO: implement get review
         let _repo = ctrl.review_repo;
         tracing::debug!("get_review: {:?}", id);
 
-        Ok(Json(Review::default()))
+        let fetched_review = sqlx::query_as!(
+            Review,
+            "SELECT * FROM reviews WHERE id = $1",
+            id
+        )
+        .fetch_one(&ctrl.pool)
+        .await;
+    
+        match fetched_review {
+            Ok(review) => Ok(Json(review)),
+            Err(_) => Err(ApiError::ResourceNotFound),
+        }
     }
 
     pub async fn act_by_id(
@@ -44,7 +56,6 @@ impl ReviewControllerV1 {
         Path(id): Path<i64>,
         Json(body): Json<ReviewByIdAction>,
     ) -> Result<Json<Review>> {
-        //TODO: implement act_by_id
         let _repo = ctrl.clone().review_repo;
         tracing::debug!("act_by_id: {:?} {:?}", id, body);
 
@@ -57,7 +68,6 @@ impl ReviewControllerV1 {
         State(ctrl): State<ReviewControllerV1>,
         Query(params): Query<ReviewParam>,
     ) -> Result<Json<ReviewGetResponse>> {
-        //TODO: implement list_reviews
         let _repo = ctrl.clone().review_repo;
         tracing::debug!("list_reviews: {:?}", params);
 
@@ -73,7 +83,6 @@ impl ReviewControllerV1 {
         State(ctrl): State<ReviewControllerV1>,
         Json(body): Json<ReviewAction>,
     ) -> Result<Json<Review>> {
-        //TODO: implement act_review
         let _repo = ctrl.clone().review_repo;
         tracing::debug!("act review {:?}", body);
 
@@ -88,22 +97,47 @@ impl ReviewControllerV1 {
     pub async fn update(&self, id: i64, params: ReviewUpdateRequest) -> Result<Json<Review>> {
         tracing::debug!("update review: {:?} {:?}", id, params);
 
-        Ok(Json(Review::default()))
+        let fetched_review = sqlx::query_as!(
+            Review,
+            "UPDATE reviews SET name = $1, image = $2, review = $3 WHERE id = $4 RETURNING *",
+            params.name,
+            params.image,
+            params.review,
+            id
+        )
+        .fetch_one(&self.pool)
+        .await;
+
+        match fetched_review {
+            Ok(review) => Ok(Json(review)),
+            Err(_) => Err(ApiError::ApiCallError("Something went wrong".to_string())),
+        }
     }
 
     pub async fn find(
         &self,
         ReviewQuery { size, bookmark, .. }: ReviewQuery,
     ) -> Result<Json<ReviewGetResponse>> {
-        let _size = size;
+        let _size = size as i64;
         let _bookmark = bookmark;
 
         tracing::debug!("find query");
 
-        Ok(Json(ReviewGetResponse::Query(QueryResponse {
-            items: vec![],
-            total_count: 0,
-        })))
+       let found_reviews = sqlx::query_as!(
+            Review,
+            "SELECT * FROM reviews ORDER BY created_at DESC LIMIT $1",
+            _size
+        )
+        .fetch_all(&self.pool)
+        .await;
+
+        match found_reviews {
+            Ok(reviews) => Ok(Json(ReviewGetResponse::Query(QueryResponse {
+                total_count: reviews.len() as i64,
+                items: reviews.into_iter().map(ReviewSummary::from).collect(),
+            }))),
+            Err(_) => Err(ApiError::ApiCallError("Something went wrong".to_string())),
+        }
     }
 
     pub async fn search_by(
@@ -115,26 +149,60 @@ impl ReviewControllerV1 {
             ..
         }: ReviewQuery,
     ) -> Result<Json<ReviewGetResponse>> {
-        let _size = size;
+        let _size = size as i64;
         let _bookmark = bookmark;
         let _name = name;
         tracing::debug!("search by");
 
-        Ok(Json(ReviewGetResponse::Query(QueryResponse {
-            items: vec![],
-            total_count: 0,
-        })))
+         let search_results = sqlx::query_as!(
+            Review,
+            "SELECT * FROM reviews WHERE name ILIKE $1 ORDER BY created_at DESC LIMIT $2",
+            format!("%{}%", _name.unwrap_or_default()),
+            _size
+        )
+        .fetch_all(&self.pool)
+        .await;
+
+        match search_results {
+            Ok(reviews) => Ok(Json(ReviewGetResponse::Query(QueryResponse {
+                total_count: reviews.len() as i64,
+                items: reviews.into_iter().map(ReviewSummary::from).collect(),
+            }))),
+           Err(_) => Err(ApiError::ApiCallError("Something went wrong".to_string())),
+        }
     }
 
     pub async fn create(&self, params: ReviewCreateRequest) -> Result<Json<Review>> {
         tracing::debug!("create review: {:?}", params);
+        let new_review = sqlx::query_as!(
+            Review,
+            "INSERT INTO reviews (name, image, review) VALUES ($1, $2, $3) RETURNING *",
+            params.name,
+            params.image,
+            params.review
+        )
+        .fetch_one(&self.pool)
+        .await;
 
-        Ok(Json(Review::default()))
+        match new_review {
+            Ok(review) => Ok(Json(review)),
+            Err(_) => Err(ApiError::ApiCallError("Something went wrong".to_string())),
+        }
     }
 
     pub async fn delete(&self, review_id: i64) -> Result<Json<Review>> {
         tracing::debug!("delete review: {:?}", review_id);
+        let found_review = sqlx::query_as!(
+            Review,
+            "DELETE FROM reviews WHERE id = $1 RETURNING *",
+            review_id
+        )
+        .fetch_one(&self.pool)
+        .await;
 
-        Ok(Json(Review::default()))
+        match found_review {
+            Ok(review) => Ok(Json(review)),
+            Err(_) => Err(ApiError::ApiCallError("Something went wrong".to_string())),
+        }
     }
 }
