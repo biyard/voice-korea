b34baf8f9cb9d9180b83dec0dccc060c0971aeec
victor138128 <yepp0517@naver.com>
2025-03-05 17:53:25 +0900

    add quota field and comment component mock page

--- packages/models/src/deliberation/deliberation_comment.rs
+++ packages/models/src/deliberation/deliberation_comment.rs
@@ -20,14 +20,52 @@ pub struct DeliberationComment {
     #[api_model(summary, action = create)]
     pub comment: String,
 
-    #[api_model(summary, one_to_many = deliberation_comments_reply)]
+    #[api_model(summary, action = create, type = JSONB, version = v0.1, nullable)]
+    pub quota: Vec<CommentQuota>,
+    #[api_model(summary, one_to_many = deliberation_comment_reply)]
     pub replies: Vec<CommentReply>,
-    #[api_model(summary, one_to_many = deliberation_comments_reply, foreign_key = comment_id, aggregator = count)]
+    #[api_model(summary, one_to_many = deliberation_comment_reply, foreign_key = comment_id, aggregator = count)]
     pub num_of_replies: i64,
+    #[api_model(summary, one_to_many = deliberation_comment_likers, foreign_key = comment_id, aggregator = count)]
+    pub num_of_likers: i64,
 }
 
 #[derive(Validate)]
-#[api_model(base = "/", table = deliberation_comments_reply)]
+#[api_model(base = "/", table = deliberation_comment_quotas)]
+pub struct CommentQuota {
+    #[api_model(summary, primary_key)]
+    pub id: i64,
+    #[api_model(summary, auto = [insert])]
+    pub created_at: i64,
+    #[api_model(summary, auto = [insert, update])]
+    pub updated_at: i64,
+
+    #[api_model(many_to_one = users, action = create)]
+    pub user_id: i64,
+    #[api_model(many_to_one = deliberation_comments, action = create)]
+    pub comment_id: i64,
+    #[api_model(summary, action = create)]
+    pub comment: String,
+}
+
+#[derive(Validate)]
+#[api_model(base = "/", table = deliberation_comment_likers)]
+pub struct CommentLikers {
+    #[api_model(summary, primary_key)]
+    pub id: i64,
+    #[api_model(summary, auto = [insert])]
+    pub created_at: i64,
+    #[api_model(summary, auto = [insert, update])]
+    pub updated_at: i64,
+
+    #[api_model(many_to_one = users, action = create)]
+    pub user_id: i64,
+    #[api_model(many_to_one = deliberation_comments, action = create)]
+    pub comment_id: i64,
+}
+
+#[derive(Validate)]
+#[api_model(base = "/", table = deliberation_comment_reply)]
 pub struct CommentReply {
     #[api_model(summary, primary_key)]
     pub id: i64,
