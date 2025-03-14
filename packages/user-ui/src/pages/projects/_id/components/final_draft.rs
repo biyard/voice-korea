a0798d7d039cdbbf2e9f7756d08042f06d73d223
victor138128 <yepp0517@naver.com>
2025-03-14 21:47:40 +0900

    fix typo

--- packages/user-ui/src/pages/projects/_id/components/final_draft.rs
+++ packages/user-ui/src/pages/projects/_id/components/final_draft.rs
@@ -11,7 +11,7 @@ pub fn FinalDraft(
     children: Element,
 ) -> Element {
     let ctrl = Controller::new(lang, project_id)?;
-    let tr: BasicInfoTranslate = translate(&lang);
+    let tr: FinalDraftTranslate = translate(&lang);
 
     rsx! {
         div { id: "final-draft", ..attributes,
@@ -40,10 +40,10 @@ impl Controller {
 }
 
 translate! {
-    BasicInfoTranslate;
+    FinalDraftTranslate;
 
     title: {
-        ko: "BasicInfo",
-        en: "BasicInfo",
+        ko: "Final Draft",
+        en: "Final Draft",
     },
 }
