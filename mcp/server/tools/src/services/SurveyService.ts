1b99b90475ef05f00e0492ebab996e399472020d
Boniface <boniface.ebuka@gmail.com>
2025-05-07 00:23:20 +0100

    Added a new tool that allows the MCP to fetch surveys that belongs to logged in user and also get the details of a survey by its ID.

--- mcp/server/tools/src/services/SurveyService.ts
+++ mcp/server/tools/src/services/SurveyService.ts
@@ -77,4 +77,66 @@ export default class SurveyService {
             };
           }
     }
+
+    public async fetchUserSurveys(question: string)
+    {
+        try {
+            const project = await makeApiCall(`/organizations/6/surveys?param-type=query&size=10&bookmark=1`, 
+              { 
+                method: 'GET',
+                headers:{
+                  Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0NyIsImV4cCI6MTc0NjU3NTY0Mywicm9sZSI6InVzZXIiLCJjdXN0b20iOnsiZW1haWwiOiJib25pZmFjZS5lYnVrYUBnbWFpbC5jb20ifX0.PHTElB8koz9vpY6TKz4AV_mMQQxEj2ztv2EFKj7ncaU"
+                }
+              })
+            if (!project) {
+              return {
+                content: [{ type: "text", text: `Unable to find your surveys` }]
+              };
+            }
+    
+            return {
+              content: [
+                {
+                  type: "text",
+                  text: `question asked is: ${question}, matching user survey data is: ${JSON.stringify(project, null, 2)}`
+                }
+              ]
+            };
+        } catch (error: any) {
+            return {
+              content: [{ type: "text", text: `Error fetching your survey: ${error.message}` }]
+            };
+          }
+    }
+
+    public async fetchUserSurveyById(id: number, question: string)
+    {
+        try {
+            const survey = await makeApiCall(`/organizations/6/surveys/${id}`, 
+              { 
+                method: 'GET',
+                headers:{
+                  Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0NyIsImV4cCI6MTc0NjU3NTY0Mywicm9sZSI6InVzZXIiLCJjdXN0b20iOnsiZW1haWwiOiJib25pZmFjZS5lYnVrYUBnbWFpbC5jb20ifX0.PHTElB8koz9vpY6TKz4AV_mMQQxEj2ztv2EFKj7ncaU"
+                }
+              })
+            if (!survey) {
+              return {
+                content: [{ type: "text", text: `Unable to find the details of this your survey with ID: ${id}` }]
+              };
+            }
+    
+            return {
+              content: [
+                {
+                  type: "text",
+                  text: `question asked is: ${question}, matching user survey by ID's data is: ${JSON.stringify(survey, null, 2)}`
+                }
+              ]
+            };
+        } catch (error: any) {
+            return {
+              content: [{ type: "text", text: `Error fetching your survey: ${error.message}` }]
+            };
+          }
+    }
 }
--- mcp/server/tools/src/tools/SurveyTools.ts
+++ mcp/server/tools/src/tools/SurveyTools.ts
@@ -24,5 +24,28 @@ export const surveyTools: Tool[] = [
           },
           required: ["question","id"]
         }
+      },
+      {
+        name: "fetch_my_surveys",
+        description: "Fetch Voice Korea's Project's surveys that are under a user's account",
+        inputSchema: {
+          type: "object",
+          properties: {
+            question: { type: "string" },
+          },
+          required: ["question"]
+        }
+      },
+      {
+        name: "fetch_details_of_my_survey_by_survey_id",
+        description: "Fetch one Voice Korea's Project's survey that is under a user's account using the survey's ID",
+        inputSchema: {
+          type: "object",
+          properties: {
+            question: { type: "string" },
+            id: { type: "number" }
+          },
+          required: ["question","id"]
+        }
       }
 ];
