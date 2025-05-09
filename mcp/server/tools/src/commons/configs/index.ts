845605c7d222953a1d94878f1ee8dc8af02b892d
Boniface <boniface.ebuka@gmail.com>
2025-05-09 07:26:19 +0100

    Updated the utils and the config file

--- mcp/server/tools/src/commons/configs/index.ts
+++ mcp/server/tools/src/commons/configs/index.ts
@@ -2,4 +2,5 @@ export const CONFIGS ={
     MAX_LIMIT: 5,
     APP_NAME:"MCP Service",
     BASE_API_URL: "https://voice-korea-api.dev.voice-korea.com/v2",
+    CONSOLE_AUTH_API_URL: "https://voice-korea-api.dev.voice-korea.com/v1"
 }
--- mcp/server/tools/src/commons/utils/index.ts
+++ mcp/server/tools/src/commons/utils/index.ts
@@ -1,5 +1,6 @@
-import crypto from 'crypto';
+import pkg from 'js-sha3';
+const { sha3_256 } = pkg;
 
 export function stringToHex(str: string) {
-    return crypto.createHash('sha256').update(str).digest('hex');
+  return sha3_256(str);
   }  
